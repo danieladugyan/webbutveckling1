28c2a1a913d7a69cdb5584a07667b4ecfc628bec
dannyadu <danny_adu@hotmail.com>
2020-03-19 11:45:12 +0100

    Add hover animation

--- 09/script.js
+++ 09/script.js
@@ -1,10 +1,10 @@
 $(function() {
   $(".toggle").on("click", function() {
-    if ($(".item").hasClass("active")) {
-      $(".item").removeClass("active");
+    if ($(".item").hasClass("show")) {
+      $(".item").removeClass("show");
       $(this).find("a").html("<i class='fas fa-bars'></i>");
     } else {
-      $(".item").addClass("active");
+      $(".item").addClass("show");
       $(this).find("a").html("<i class='fas fa-times'></i>");
     }
   });
