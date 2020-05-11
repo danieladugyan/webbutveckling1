617d103192775223767df447952823a35a119b26
dannyadu <danny_adu@hotmail.com>
2020-05-11 09:05:11 +0200

    Moderately complete version

--- 09/js/script.js
+++ 09/js/script.js
@@ -10,11 +10,55 @@ $(function() {
   });
 });
 
-/*function toggleMenu() {
-  let x = document.querySelector(".navbar");
-  if (x.className === "navbar") {
-    x.className += " phone";
+// MY CODE
+
+/*function toggleMenu() { // doesn't work properly
+  let toggle = document.querySelector(".toggle");
+  let item = document.querySelector(".item");
+
+  let i = document.querySelector("i");
+  i.classList.add("fas")
+
+  if (item.classList.contains("show")) {
+    item.classList.remove("show");
+    i.classList.add("fa-bars");
+    toggle.children.querySelector("a").appendChild(i);
   } else {
-    x.className = "navbar";
+    item.classList.add("show");
+    i.classList.add("fa-times");
+    toggle.children.querySelector("a").removeChild(i);
   }
 }*/
+
+let date = new Date();
+
+let yearsFromNow = 1; // 1 year
+let timeFromNow = 1; // 1 month
+
+dateTime =`${date.getFullYear()}-${(date.getMonth()+1).toString().
+  padStart(2, 0)}-${date.getDate().toString().padStart(2, 0)}T${date.getHours()}:${
+  date.getMinutes().toString().padStart(2, 0)}`;
+
+dateTimeMax =`${date.getFullYear()}-${((date.getMonth()+1)+timeFromNow).toString().
+  padStart(2, 0)}-${(date.getDate()+1).toString().padStart(2, 0)}T00:00`;
+
+dateMax = `${date.getFullYear()+yearsFromNow}-${(date.getMonth()+1).toString().
+  padStart(2, 0)}-${date.getDate().toString().padStart(2, 0)}`;
+
+date = `${date.getFullYear()}-${(date.getMonth()+1).toString().
+  padStart(2, 0)}-${date.getDate().toString().padStart(2, 0)}`;
+
+let dateFields = document.querySelectorAll("input[type=date]");
+let dateTimeFields = document.querySelectorAll("input[type=datetime-local]");
+
+dateFields.forEach((field) => {
+  field.value = date;
+  field.min = date;
+  field.max = dateMax;
+});
+
+dateTimeFields.forEach((field) => {
+  field.value = dateTime;
+  field.min = dateTime;
+  field.max = dateTimeMax;
+});
